cad14b567400e18c15296adc2dcde602fa150343
Dor Bar <dor4231@gmail.com>
2018-10-07 23:04:10 +0300

    feat: Create the Navigation componenet
    This component will have a list of all the available sections to the
    current logged in user.

---
+++ nethood-react/src/comps/Navigation.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Navigation extends Component {
+  render() {
+    return (
+      <nav className=''>
+        <h2>Main menu:</h2>
+        <ul>
+            <li>Home</li>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+export default Navigation;
