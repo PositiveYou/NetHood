d8ad41754ee801d759b58b0fbe6a3d418f685fc7
Dor Bar <dor4231@gmail.com>
2018-10-15 23:49:01 +0300

    feat: Make the basic API work without errors

--- nethood/api/views.py
+++ nethood/api/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
 from .models import Event, User
 
 
 def get_user(request):
     print(request)
-    return HttpResponse({"name": "dor", "email": request})
+    return JsonResponse({"name": "dor", "email": request.user.username})
 
 
 def get_events(request):
     print(request)
-    return HttpResponse(Event.objects.all())
+    return HttpResponse(Event.objects.order_by('-pub_date')[:5])
