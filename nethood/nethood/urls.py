c572c4bf4b6e18f5cd496a7463c5a03831cbebba
Marina Rappoport <rappoportmarina@gmail.com>
2018-10-16 22:05:34 +0300

    added social auth support to project

--- nethood/nethood/urls.py
+++ nethood/nethood/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('', include('frontend.urls')),
     path('api/', include('api.urls')),
     path('admin/', admin.site.urls),
+    path('auth/', include('social_django.urls', namespace='social')),
 ]
